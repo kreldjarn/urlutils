7b16f183379fe4d08df413c4d567b03fc820c55f
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 15:04:30 +0000

    urlutils -> string, version 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='urlutils',
     version='0.1.1',
-    packages=['urlutils',],
+    packages=['urlutils','urlutils.string'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )

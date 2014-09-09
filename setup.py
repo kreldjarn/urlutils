27b4b02f31882049f9d01bfce3dbdc632e434543
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 15:23:19 +0000

    Updated version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='urlutils',
-    version='0.0.6',
+    version='0.0.7',
     packages=['urlutils',],
     include_package_data = True,
     license='BSD Licence',

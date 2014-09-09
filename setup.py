59b6f3cbc5d5bb5bf25dceedca9b725e2b6c2740
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 15:07:55 +0000

    MANIFEST.in

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='urlutils',
-    version='0.1.1',
-    packages=['urlutils','urlutils.string'],
+    version='0.1.3',
+    packages=['urlutils',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )

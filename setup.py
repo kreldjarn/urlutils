aadf88c63297cf6ea71699a0da01bc220d2f3086
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 14:27:38 +0000

    Removed version from setup

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 
 setup(
     name='urlutils',
-    version='0.1',
     packages=['urlutils',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )

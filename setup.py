2f46e8329141380d767dea2b1eda070bdb12c5f9
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 14:00:03 +0000

    init commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='urlutils',
+    version='0.1',
+    packages=['urlutils',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)

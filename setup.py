999ddbd0b862eb79dabe9fdb3abe9b67c7b80a01
Kristján Eldjárn <kristjan@eldjarn.net>
2014-09-09 15:10:26 +0000

    Version 0.1.3 -> 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='urlutils',
-    version='0.1.3',
+    version='0.1.4',
     packages=['urlutils',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
